a5652b1f6d667bd00deb755866fb17ec91ad83e3
ngwwm <ngwmm@ha.org.hk>
2017-02-23 16:24:30 +0800

    Event popover now keep alive while popover is being hovered.

--- src/Calendar/wwwroot/js/calendar.js
+++ src/Calendar/wwwroot/js/calendar.js
@@ -24,10 +24,28 @@ $(function () {
 $(function(){
     $('body').tooltip( {selector: '[data-toggle=tooltip]'} );
 });
-
+/*
 $(function () {
     $('[data-toggle="popover"]').popover();
 });
-
+*/
+$(function () {
+    $('[data-toggle="popover"]').popover({ trigger: "manual", html: true, animation: false })
+        .on("mouseenter", function () {
+            var _this = this;
+            $(this).popover("show");
+            $(".popover").on("mouseleave", function () {
+                $(_this).popover('hide');
+            });
+        })
+        .on("mouseleave", function () {
+            var _this = this;
+            setTimeout(function () {
+                if (!$(".popover:hover").length) {
+                    $(_this).popover("hide");
+                }
+            }, 300);
+        });
+});
 $(document).ready(jqUpdateSize);    // When the page first loads
 $(window).resize(jqUpdateSize);     // When the browser changes size
