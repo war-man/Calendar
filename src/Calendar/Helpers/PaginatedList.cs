cef3840884e4ce9cf922cbeb9b766446e6357c2c
ngwwm <ngwmm@ha.org.hk>
2017-03-28 12:21:09 +0800

    merge feature-mc170322a with mc170320a

--- src/Calendar/Helpers/PaginatedList.cs
+++ src/Calendar/Helpers/PaginatedList.cs
@@ -9,12 +9,14 @@ namespace Calendar.Helpers
     public class PaginatedList<T> : List<T>
     {
         public int PageIndex { get; private set; }
+        public int PageSize { get; private set; }
         public int TotalPages { get; private set; }
         public int TotalCount { get; private set; }
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
+            PageSize = pageSize;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             TotalCount = count;
 
