f36c7c0f37ca7b019332f4403651e81a8f87d46f
NWW196 <NWW196@HA4-A2-W01>
2017-08-14 16:40:27 +0800

    Update to Visual Studio 2017 + .NetCoreApp1.1

--- src/Calendar/Startup.cs
+++ src/Calendar/Startup.cs
@@ -33,7 +33,8 @@ namespace Calendar
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                // builder.AddUserSecrets();
+                builder.AddUserSecrets<Startup>();
             }
 
             builder.AddEnvironmentVariables();
